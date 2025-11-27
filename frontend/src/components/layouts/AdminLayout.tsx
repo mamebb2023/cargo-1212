bdfffff54bcc1f7e37296e1f5180879116231ff9
Mohammednur <mamebb2023@gmail.com>
2025-11-27 20:20:08 +0300

    removed unused consts

--- frontend/src/components/layouts/AdminLayout.tsx
+++ frontend/src/components/layouts/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import {
   LayoutDashboard,
@@ -126,4 +126,3 @@ export default function AdminLayout() {
     </div>
   );
 }
-
